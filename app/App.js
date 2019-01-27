877ca755dd549049d641c5e2b177f5e8e01d84a5
Victor Lopes <contato_victorlopes@hotmail.com>
2019-01-27 02:44:02 -0300

    adicionando apis

---
+++ app/App.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//renderizando html com jsx
+ReactDOM.render(<h1>Hello, World mother fucker!</h1>, document.getElementById('app'));
